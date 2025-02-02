c371e49a046195ec8893e2224f182fa76c9c4d90
Jeongyounghyeon <snorlax1106@naver.com>
2025-02-02 17:18:43 +0900

    feat: refresh-token cleaning 작업 추가
    Scheduler를 이용하여 매일 새벽 3시 정각에 만료시간이 지난 토큰 제거 기능 추가

--- src/main/java/com/example/INFO/user/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/INFO/user/repository/RefreshTokenRepository.java
@@ -3,10 +3,11 @@ package com.example.INFO.user.repository;
 import com.example.INFO.user.model.entity.RefreshTokenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.time.LocalDateTime;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, Long> {
 
-    Optional<RefreshTokenEntity> findByValue(String value);
     boolean existsByValue(String value);
+
+    void deleteByExpirationBefore(LocalDateTime now);
 }
