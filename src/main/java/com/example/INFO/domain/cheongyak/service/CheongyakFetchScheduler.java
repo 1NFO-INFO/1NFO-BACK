14e06ee31961e698efaebe7933c44ee0334aa27f
Jeongyounghyeon <snorlax1106@naver.com>
2025-02-11 22:02:26 +0900

    feat: 청약 fetch log 정리

--- src/main/java/com/example/INFO/domain/cheongyak/service/CheongyakFetchScheduler.java
+++ src/main/java/com/example/INFO/domain/cheongyak/service/CheongyakFetchScheduler.java
@@ -23,51 +23,51 @@ public class CheongyakFetchScheduler {
 
     private void fetchAptCheongyak() {
         try {
-            log.info("아파트 청약 상세정보 fetch 작업 시작");
+            log.info("청약 상세정보 fetch 작업 시작 - APT");
             cheongyakDetailsFetchService.fetchAptData();
-            log.info("아파트 청약 상세정보 fetch 작업 성공 - APT");
+            log.info("청약 상세정보 fetch 작업 성공 - APT");
         } catch (Exception e) {
-            log.error("아파트 청약 상세정보 fetch 작업 실패 - APT", e);
+            log.error("청약 상세정보 fetch 작업 실패 - APT", e);
         }
     }
 
     private void fetchUrbtyOfctlCheongyak() {
         try {
-            log.info("오피스텔/도시형/민간임대/생활숙박시설 청약 상세정보 fetch 작업 시작");
+            log.info("청약 상세정보 fetch 작업 시작 - 오피스텔/도시형/민간임대/생활숙박시설");
             cheongyakDetailsFetchService.fetchUrbtyOfctlData();
-            log.info("오피스텔/도시형/민간임대/생활숙박시설 청약 상세정보 fetch 작업 성공 - APT");
+            log.info("청약 상세정보 fetch 작업 성공 - 오피스텔/도시형/민간임대/생활숙박시설");
         } catch (Exception e) {
-            log.error("오피스텔/도시형/민간임대/생활숙박시설 청약 상세정보 fetch 작업 실패 - APT", e);
+            log.error("청약 상세정보 fetch 작업 실패 - 오피스텔/도시형/민간임대/생활숙박시설", e);
         }
     }
 
     private void fetchRemndrCheongyak() {
         try {
-            log.info("APT 무순위/잔여세대 청약 상세정보 fetch 작업 시작");
+            log.info("청약 상세정보 fetch 작업 시작 - APT 무순위/잔여세대");
             cheongyakDetailsFetchService.fetchRemndrData();
-            log.info("APT 무순위/잔여세대 청약 상세정보 fetch 작업 성공");
+            log.info("청약 상세정보 fetch 작업 성공 - APT 무순위/잔여세대");
         } catch (Exception e) {
-            log.error("APT 무순위/잔여세대 청약 상세정보 fetch 작업 실패", e);
+            log.error("청약 상세정보 fetch 작업 실패 - APT 무순위/잔여세대", e);
         }
     }
 
     private void fetchPblPvtRentCheongyak() {
         try {
-            log.info("공공지원 민간임대 청약 상세정보 fetch 작업 시작");
+            log.info("청약 상세정보 fetch 작업 시작 - 공공지원 민간임대");
             cheongyakDetailsFetchService.fetchPblPvtRentData();
-            log.info("공공지원 민간임대 청약 상세정보 fetch 작업 성공");
+            log.info("청약 상세정보 fetch 작업 성공 - 공공지원 민간임대");
         } catch (Exception e) {
-            log.error("공공지원 민간임대 청약 상세정보 fetch 작업 실패", e);
+            log.error("청약 상세정보 fetch 작업 실패 - 공공지원 민간임대", e);
         }
     }
 
     private void fetchOptCheongyak() {
         try {
-            log.info("임의공급 청약 상세정보 fetch 작업 시작");
+            log.info("청약 상세정보 fetch 작업 시작 - 임의공급");
             cheongyakDetailsFetchService.fetchOptData();
-            log.info("임의공급 청약 상세정보 fetch 작업 성공");
+            log.info("청약 상세정보 fetch 작업 성공 - 임의공급");
         } catch (Exception e) {
-            log.error("임의공급 청약 상세정보 fetch 작업 실패", e);
+            log.error("청약 상세정보 fetch 작업 실패 - 임의공급", e);
         }
     }
 }
