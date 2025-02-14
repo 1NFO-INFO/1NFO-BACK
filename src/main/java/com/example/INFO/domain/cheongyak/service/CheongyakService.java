e25b24f72e9f5b8f84dd1c854187cc08ba6ba94d
Jeongyounghyeon <snorlax1106@naver.com>
2025-02-14 18:27:52 +0900

    feat: 청약 리스트 조회 기본 정렬 최신 공고순으로 설정

--- src/main/java/com/example/INFO/domain/cheongyak/service/CheongyakService.java
+++ src/main/java/com/example/INFO/domain/cheongyak/service/CheongyakService.java
@@ -5,7 +5,9 @@ import com.example.INFO.domain.cheongyak.model.entity.CheongyakDetailsEntity;
 import com.example.INFO.domain.cheongyak.repository.CheongyakDetailsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +20,20 @@ public class CheongyakService {
     private final CheongyakDetailsRepository cheongyakDetailsRepository;
 
     public Page<CheongyakDetailsDto> list(Pageable pageable, List<String> housingTypes) {
+        pageable = addDefaultSorting(pageable);
+
         Specification<CheongyakDetailsEntity> spec =
                 Specification.where(housingTypeIn(housingTypes));
 
         return cheongyakDetailsRepository.findAll(spec, pageable).map(CheongyakDetailsDto::fromEntity);
+    }
+
+    private Pageable addDefaultSorting(Pageable pageable) {
+        if (pageable.getSort().isEmpty()) {
+            return PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(),
+                    Sort.by(Sort.Order.desc("recruitmentNoticeDate")));
+        }
+        return pageable;
     }
 
     private static Specification<CheongyakDetailsEntity> housingTypeIn(List<String> housingTypes) {
