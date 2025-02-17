1735824be43f794b4493bfa5089f569f6b9351d3
배석현 <bjh4966@naver.com>
2025-02-17 21:49:41 +0900

    [REFACT] UnauthorizedException 수정

--- src/main/java/com/example/INFO/global/exception/UnauthorizedException.java
+++ src/main/java/com/example/INFO/global/exception/UnauthorizedException.java
@@ -1,10 +1,7 @@
 package com.example.INFO.global.exception;
 
 import com.example.INFO.global.payload.ErrorCode;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class UnauthorizedException extends CustomException {
     public UnauthorizedException(ErrorCode errorCode) {
         super(errorCode);
